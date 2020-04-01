1e3d23d50dcc0445fb035a4b46e91c90aa9849c0
PipeWarp <62858343+PipeWarp@users.noreply.github.com>
2020-04-01 02:19:29 +0100

    Update simplenfl.h

--- simplenfl.h
+++ simplenfl.h
@@ -1,11 +1,8 @@
 void loadsprite(int screen, int ramslot, int vramslot, int width, int height, const char* dir, const char* dir2, bool transflag);
 
 void loadsprite(int screen, int ramslot, int vramslot, int width, int height, const char* dir, const char* dir2, bool transflag){
-    // Load our Tiled Sprite
-NF_LoadSpriteGfx(dir, ramslot, width, height);// Tempy!
+NF_LoadSpriteGfx(dir, ramslot, width, height);
 NF_LoadSpritePal(dir2, ramslot);
-
-// Transfer our sprite to VRAM
 NF_VramSpriteGfx(screen, ramslot, vramslot, transflag);
 NF_VramSpritePal(screen, ramslot, vramslot);
 }
