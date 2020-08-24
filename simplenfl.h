7f762d9aa6ec949ab414fddc5793158cf7442a11
Ferdian <ferdian.artwork@gmail.com>
2020-08-24 13:07:25 +0700

    adding KeyPhase for touch and key input

--- simplenfl.h
+++ simplenfl.h
@@ -9,28 +9,19 @@
 //The Docs/wiki is in the github repository
 //Have fun!
 
-u16 Pressed;
-u16 Held;
-u16 Released;
+#pragma once
+
+enum Key {
+    UP, DOWN, LEFT, RIGHT,
+    START, SELECT,
+    A, B, X, Y,
+    R, L
+};
+
+enum KeyPhase { release, press, held };
+
 touchPosition Stylus;
-
-const char *A = "A";
-const char *B = "B";
-const char *X = "X";
-const char *Y = "Y";
-const char *UP = "UP";
-const char *DOWN = "DOWN";
-const char *LEFT = "LEFT";
-const char *RIGHT = "RIGHT";
-const char *R = "R";
-const char *L = "L";
-const char *SELECT = "SELECT";
-const char *START = "START";
-const char *PRESS = "press";
-const char *HELD = "held";
-const char *RELEASE = "release";
-
-
+touchPosition PrevStylus; // previous frame stylus position
 
 void loadsprite(int screen, int ramslot, int vramslot, int width, int height, const char *dir, const char *dir2,
                 bool transflag);
@@ -66,359 +57,43 @@ playandloadsound(const char *file, int channel, int freq, int sampleform, int vo
     NF_PlayRawSound(channel, vol, pan, loop, loopstart);
 }
 
+bool getTouch(KeyPhase phase);
 
-bool istouched(int x, int y, int width, int height);
+bool getTouch(KeyPhase phase) {
+    touchRead(&Stylus);
+    
+    switch (phase){
+        case KeyPhase::release:
+            // store prev stylus because px,py return 0 when KeysUp()
+            if (KEY_TOUCH & keysHeld()) PrevStylus = Stylus;
 
-bool istouched(int x, int y, int width, int height) {
-    scanKeys();// Scan for Input
-    touchRead(&Stylus);// Read Stylus data
+            if (KEY_TOUCH & keysUp()) {
+                Stylus = PrevStylus;
+                return true;
+            }
+            else break;
+        
+        case KeyPhase::press:
+            return (KEY_TOUCH & keysDown());
+            break;
 
+        case KeyPhase::held:
+            return (KEY_TOUCH & keysHeld());
+            break;
+    }
 
-    if (KEY_TOUCH & keysDown()) {
+    return false;
+}
+
+bool getTouchRect(int x, int y, int width, int height, KeyPhase phase);
+
+bool getTouchRect(int x, int y, int width, int height, KeyPhase phase) {
+    if (getTouch(phase)) {
         int mathx = x + width;
         int mathy = y + height;
         return Stylus.px > x && Stylus.px < mathx && Stylus.py > y && Stylus.py < mathy;
     }
     return false;
-
-}
-
-bool ispressed(const char *button, const char *type);
-
-bool ispressed(const char *button, const char *type) {
-    scanKeys();
-    Pressed = keysDown();
-    Held = keysHeld();
-    Released = keysUp();
-    switch(button){
-        case A:
-        switch(type){
-            case PRESS:
-            if (KEY_A & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_A & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_A & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case B:
-        switch(type){
-            case PRESS:
-            if (KEY_B & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_B & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_B & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case X:
-        switch(type){
-            case PRESS:
-            if (KEY_X & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_X & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_X & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case Y:
-        switch(type){
-            case PRESS:
-            if (KEY_Y & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_Y & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_Y & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-
-        case L:
-        switch(type){
-            case PRESS:
-            if (KEY_L & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_L & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_L & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-         
-        case R:
-        switch(type){
-            case PRESS:
-            if (KEY_R & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_R & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_R & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-        
-
-        case UP:
-        switch(type){
-            case PRESS:
-            if (KEY_UP & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_UP & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_UP & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case DOWN:
-        switch(type){
-            case PRESS:
-            if (KEY_DOWN & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_DOWN & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_DOWN & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case LEFT:
-        switch(type){
-            case PRESS:
-            if (KEY_LEFT & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_LEFT & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_LEFT & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case RIGHT:
-        switch(type){
-            case PRESS:
-            if (KEY_RIGHT & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_RIGHT & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_RIGHT & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case SELECT:
-        switch(type){
-            case PRESS:
-            if (KEY_SELECT & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_SELECT & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_SELECT & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-        case START:
-        switch(type){
-            case PRESS:
-            if (KEY_START & Pressed) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case HELD:
-            if (KEY_START & Held) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-            case RELEASE:
-            if (KEY_START & Released) {
-                return true;
-            } else {
-                return false;
-            }
-            break;
-
-        }
-        break;
-
-    }
-    
 }
 
 bool overlap(float x1, float y1, float x2, float y2, float x3, float y3, float x4, float y4);
@@ -430,5 +105,181 @@ bool overlap(float x1, float y1, float x2, float y2, float x3, float y3, float x
         (x3 <= x2 && x2 <= x4 && y3 <= y2 && y2 <= y4)) {
         return true;
     }
-    
+
+    return false;
+}
+
+bool getKeys(Key key, KeyPhase phase);
+
+bool getKeys(Key key, KeyPhase phase) {
+    switch (key) {
+        case Key::A:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_A & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_A & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_A & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::B:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_B & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_B & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_B & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::X:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_X & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_X & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_X & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::Y:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_Y & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_Y & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_Y & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::L:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_L & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_L & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_L & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::R:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_R & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_R & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_R & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::UP:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_UP & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_UP & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_UP & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::DOWN:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_DOWN & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_DOWN & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_DOWN & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::LEFT:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_LEFT & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_LEFT & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_LEFT & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::RIGHT:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_RIGHT & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_RIGHT & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_RIGHT & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::SELECT:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_SELECT & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_SELECT & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_SELECT & keysHeld());
+                    break;
+            }
+            break;
+        
+        case Key::START:
+            switch (phase) {
+                case KeyPhase::release:
+                    return (KEY_START & keysUp());
+                    break;
+                case KeyPhase::press:
+                    return (KEY_START & keysDown());
+                    break;
+                case KeyPhase::held:
+                    return (KEY_START & keysHeld());
+                    break;
+            }
+            break;    
+    }
+    return false;
 }
